8ca18cdd3241915ae6210e7f1f4d22cb676035ea
Steven813 <63978658+Steven813@users.noreply.github.com>
2020-07-13 02:20:57 +0200

    Add files via upload

---
+++ SDP info-c.lua
@@ -0,0 +1,32 @@
+-- simple /info script by steven dodge productions
+-- consider adding me on discord Michael Production#0301  for my future scripts
+-- if a person do /info in chat it will show them info about your server
+-- you can clear some message if you dont want them
+-- if you have any suggestions for commands add me on discord Michael Production#0301
+
+
+-- You can add other messages by just doung msg(text here: another text here) you can do that in all message just follow the template. 
+--When this is exectuted, it will execute a function, doing the msg function to the client
+RegisterCommand('info', function()
+    msg("Discord: put your discord link")
+    msg("Website: website link")
+    msg("teamspeak: teamspeak link")
+    msg("donations: link")
+	msg("youtube: subscribe to michael productions"
+end, false)
+
+-- Do not touch anything down here.
+function msg(text)
+    -- You can change the server text to anything you want but dont touch other things
+    TriggerEvent("chatMessage",  "[server]", {255,0,0}, text)
+end
+
+
+-- i have many scripts coming
+-- i dint release this on the fivem forums for some reasons
+-- if you are a developer dm on discord aswell Michael Production#0301(to join my developers team)
+-- if you need a developer dm me on discord aswell Michael Production#0301(to get developers)
+-- if you need help dm on Michael Production#0301
+-- yeah stop reading now lol
+-- if u are stupid dm on discord
+-- DO NOT READ ALL OF THIS
---
+++ resource.lua
@@ -0,0 +1,8 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+--[[
+    do not change anything in here you idiot!
+        what do u want here go start you fucking script and enter your server 
+            you idiot!
+--]]
+client_script 'SDP info-c.lua' 
